50100f4934933119ba2deee9c10350f5b917201c
Aiman khatoon <113781674+aimankhatoon1@users.noreply.github.com>
2023-09-25 21:58:05 +0500

    Add files via upload

---
+++ CC_Lab3_form3_Fa20-bcs-017.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_TASK3_Click(object sender, EventArgs e)
+        {
+
+            String var = richTextBox1.Text;
+            // split the input on the basis of space
+            String[] words = var.Split(' ');
+            // Regular Expression for variables
+            Regex regex1 = new Regex(@"\b[tmTm]\w+");
+            for (int i = 0; i < words.Length; i++)
+            {
+                Match match1 = regex1.Match(words[i]);
+                if (match1.Success)
+                {
+                    richTextBox2.Text += words[i] + " ";
+                }
+                else
+                {
+                    MessageBox.Show("invalid " + words[i]);
+                }
+            }
+        }
+    }
+}
